ea4d02c1c735abe3a3f158fc939416060b2ddc3d
Tony Rodrigues <88041342+TonyRodIv@users.noreply.github.com>
2023-04-23 19:17:49 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+
+let result = document.getElementById('result')
+let contResul = document.getElementById('contResul')
+let notaFinal = document.getElementById('notaFinal')
+contResul.style.display = 'none'
+
+function mediaPonderada() {
+    contResul.style.display = 'flex'
+    let av1 = document.getElementById('av1').value
+    let av2 = document.getElementById('av2').value
+    let av3 = document.getElementById('av3').value
+    let avt = document.getElementById('avt').value
+    let peso1 = 2.5;
+    let peso2 = 2.5;
+    let peso3 = 2;
+    let peso4 = 3;
+    let somaPesos = peso1 + peso2 + peso3 + peso4;
+    let media = (av1 * peso1 + av2 * peso2 + av3 * peso3 + avt * peso4) / somaPesos;
+    console.log(media)
+    result.innerHTML = media
+    if(media<6){
+        notaFinal.style.color = '#630101'
+    }else{
+        notaFinal.style.color = '#015658'
+    }
+    return media;
+}
